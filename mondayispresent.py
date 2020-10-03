8d52ed97de583eb60898c32da51014335245d8ae
Kartik Ganamur <kganamur@gmail.com>
2020-10-03 12:54:43 -0700

    Fourth commit

---
+++ mondayispresent.py
@@ -0,0 +1,12 @@
+mycalender = "Monday\t9:10-10:15 AM.\tLanguage Arts\n\t10:35-11:40 AM.\tSocial Studies\n\t12:10-1:15 PM.\tScience\n"+"Tuesday\t9:10-10:15 AM.\tMath\n\t10:35-11:40 AM.\tOrchestra\n\t12:10-!:15 PM.\tPhysical Fitness\n"+"Wednesday 9:10-9:30 AM.\t Homeroom\n"+"Thursday 9:10-10:15 AM.\tLanguage Arts\n\t10:35-11:40 AM.\tSocial Studies\n\t12:10-1:15 PM.\tScience\n"+"Friday\t9:10-10:15 AM.\tMath\n\t10:35-11:40 AM.\tOrchestra\n\t12:10-!:15 PM.\tPhysical Fitness\n"
+print("My calender is:\n",mycalender)
+if "Monday" in mycalender:
+    print("Monday is Present")
+else:
+    print("Monday is not present")
+    
+          
+    
+
+
+
